c833083cc5e75d3bffc5fd32a3f276a875d0b464
Martin Darras <darras.martin@gmail.com>
2020-08-12 16:57:15 +0100

    Created Controllers and routes for Restaurant and Review models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "+7757771496", category: "belgian" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "+4457771496", category: "italian" }
+yen_burger = { name: "Yen Burger", address: "1B Southwark St", phone_number: "+3357771496", category: "french" }
+tower_mangal = { name: "Tower Mangal", address: "Tower Bridge Rd", phone_number: "+2257771496", category: "japanese" }
+kalamaki = { name: "Kalamaki", address: "187 Hercules Road", phone_number: "+1157771496", category: "japanese" }
+
+[dishoom, pizza_east, yen_burger, tower_mangal, kalamaki].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
